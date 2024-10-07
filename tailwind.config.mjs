33c748576cc899cd9d859861db7c03e070e8dfea
Sakarias <post@sakarias.no>
2024-10-07 18:01:21 +0200

    Get things mostly production ready (#17)
    * Tracking
    * Frontpage with articles
    * Initial article content supporrt

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,7 +7,28 @@ export default {
         background: "#0E0F16",
         backgroundSecondary: "#131518",
       },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            color: theme("colors.white"),
+            a: {
+              color: theme("colors.blue.400"),
+              "&:hover": {
+                color: theme("colors.blue.600"),
+              },
+            },
+            "h1, h2, h3, h4": {
+              color: theme("colors.white"),
+            },
+            code: {
+              color: theme("colors.white"),
+            },
+            "blockquote p:first-of-type::before": false,
+            "blockquote p:last-of-type::after": false,
+          },
+        },
+      }),
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 };
