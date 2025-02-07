bf9198fcb50fe3bb5fea2d04d566b570eb8bd824
Nicolai Rosdahl Tellefsen <niccofyren@gmail.com>
2025-02-07 22:13:51 +0100

    feat: functional practical info and faq framework

--- src/types/info.ts
+++ src/types/info.ts
@@ -1,13 +1,28 @@
 import type { MinimalApiPage } from "./utils";
 
+export interface FaqSnippet {
+  title: string;
+  body?: string;
+  url: string;
+}
+
+export interface InfoPageSnippet {
+  title: string;
+  url: string;
+}
+
 export interface InfoPage extends MinimalApiPage {
   meta: {
     url: string;
   } & MinimalApiPage["meta"];
+  title: string;
+  body?: string;
+  intro?: string;
+  pages?: InfoPageSnippet[];
+  faq?: FaqSnippet[];
 }
+
 export interface FaqPage extends MinimalApiPage {
-  meta: {
-    url: string;
-  } & MinimalApiPage["meta"];
+  title: string;
+  body?: string;
 }
-export interface FaqEntry extends MinimalApiPage {}
