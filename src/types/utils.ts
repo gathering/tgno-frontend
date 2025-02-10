8b1329fa4a7e591442c5016913f91ffcf852d49d
niccofyren <niccofyren@gmail.com>
2025-02-10 23:45:11 +0100

    feat: add MVP of praktisk section with faq (#135)

---
+++ src/types/utils.ts
@@ -0,0 +1,14 @@
+export interface MinimalApiPage {
+  id: number;
+  meta: {
+    type: string;
+    detail_url: string;
+    html_url: string;
+    slug: string;
+    first_published_at: string;
+    seo_title: string;
+    search_description: string;
+    locale?: "en" | "no";
+  };
+  title: string;
+}
