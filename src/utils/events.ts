8c26ed3d5f07e4766255ae192ace2f3e12d96607
niccofyren <niccofyren@gmail.com>
2025-03-08 20:52:58 +0100

    chore: remove unused client side event sorting (#163)

--- src/utils/events.ts
+++ src/utils/events.ts
@@ -31,65 +31,60 @@ export const fetchEvents = async ({
   const data = await response.json();
   const pad = (number: number) => String(number).padStart(2, "0");
 
-  return (
-    data
-      // TODO: Remove once backend sorts correct by default
-      .sort((a, b) => new Date(a.start).getTime() - new Date(b.start).getTime())
-      .map((event) => {
-        const startObj = new Date(event.start);
-        const endObj = new Date(event.end);
-        const reqStartObj = reqStart ? new Date(reqStart) : null;
-        const reqEndObj = reqEnd ? new Date(reqEnd) : null;
+  return data.map((event) => {
+    const startObj = new Date(event.start);
+    const endObj = new Date(event.end);
+    const reqStartObj = reqStart ? new Date(reqStart) : null;
+    const reqEndObj = reqEnd ? new Date(reqEnd) : null;
 
-        const newEvent: Event = {
-          ...event,
-          tags: event.tags
-            .sort((a, b) => a.name.localeCompare(b.name))
-            .sort((a, b) => TAG_TYPE_PRIO[a.type] - TAG_TYPE_PRIO[b.type])
-            .map((tag) => ({
-              ...tag,
-              secondary: tag.type !== "location",
-              hidden: tag.slug === "meta",
-            })),
-          facts: {
-            immediate: event.start === event.end,
-            sameDay: startObj.getDate() === endObj.getDate(),
-            ended: Date.now() > endObj.getTime(),
-            extendingQuery: "none",
-          },
-          strings: {
-            startTime: `${pad(startObj.getHours())}:${pad(startObj.getMinutes())}`,
-            endTime: `${pad(endObj.getHours())}:${pad(endObj.getMinutes())}`,
-            startDate: startObj.toLocaleDateString("no-NO", {
-              day: "numeric",
-              weekday: "long",
-            }),
-            endDate: endObj.toLocaleDateString("no-NO", {
-              day: "numeric",
-              weekday: "long",
-            }),
-            duration: `${pad(startObj.getHours())}:${pad(startObj.getMinutes())} - ${pad(endObj.getHours())}:${pad(endObj.getMinutes())}`,
-          },
-        };
+    const newEvent: Event = {
+      ...event,
+      tags: event.tags
+        .sort((a, b) => a.name.localeCompare(b.name))
+        .sort((a, b) => TAG_TYPE_PRIO[a.type] - TAG_TYPE_PRIO[b.type])
+        .map((tag) => ({
+          ...tag,
+          secondary: tag.type !== "location",
+          hidden: tag.slug === "meta",
+        })),
+      facts: {
+        immediate: event.start === event.end,
+        sameDay: startObj.getDate() === endObj.getDate(),
+        ended: Date.now() > endObj.getTime(),
+        extendingQuery: "none",
+      },
+      strings: {
+        startTime: `${pad(startObj.getHours())}:${pad(startObj.getMinutes())}`,
+        endTime: `${pad(endObj.getHours())}:${pad(endObj.getMinutes())}`,
+        startDate: startObj.toLocaleDateString("no-NO", {
+          day: "numeric",
+          weekday: "long",
+        }),
+        endDate: endObj.toLocaleDateString("no-NO", {
+          day: "numeric",
+          weekday: "long",
+        }),
+        duration: `${pad(startObj.getHours())}:${pad(startObj.getMinutes())} - ${pad(endObj.getHours())}:${pad(endObj.getMinutes())}`,
+      },
+    };
 
-        if (newEvent.facts.immediate) {
-          newEvent.strings.duration = `${newEvent.strings.startTime}`;
+    if (newEvent.facts.immediate) {
+      newEvent.strings.duration = `${newEvent.strings.startTime}`;
+    }
+
+    if (reqStartObj && reqEndObj) {
+      if (endObj.getTime() > reqEndObj.getTime()) {
+        newEvent.facts.extendingQuery = "end";
+      }
+      if (startObj.getTime() < reqStartObj.getTime()) {
+        if (newEvent.facts.extendingQuery === "end") {
+          newEvent.facts.extendingQuery = "both";
+        } else {
+          newEvent.facts.extendingQuery = "start";
         }
+      }
+    }
 
-        if (reqStartObj && reqEndObj) {
-          if (endObj.getTime() > reqEndObj.getTime()) {
-            newEvent.facts.extendingQuery = "end";
-          }
-          if (startObj.getTime() < reqStartObj.getTime()) {
-            if (newEvent.facts.extendingQuery === "end") {
-              newEvent.facts.extendingQuery = "both";
-            } else {
-              newEvent.facts.extendingQuery = "start";
-            }
-          }
-        }
-
-        return newEvent;
-      })
-  );
+    return newEvent;
+  });
 };
