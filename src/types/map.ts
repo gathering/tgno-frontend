e2207d6ea91ea1859ec9a3fb7ece6047bd694f2a
Nicolai Rosdahl Tellefsen <niccofyren@gmail.com>
2025-04-14 16:10:00 +0200

    feat(map): add enough functionality for v1

--- src/types/map.ts
+++ src/types/map.ts
@@ -15,11 +15,39 @@ export interface PartialMapItem {
   height: number;
   polygon?: Cords[];
   presentation?: {
+    /**
+     * Control if item should show up in overall map navigation
+     */
     showInNavigation?: boolean;
+    /**
+     * Control if item should show detail popup when focused
+     */
+    showPopup?: boolean;
+    /**
+     * Control if icon should be shown in normal map view
+     */
     mapIcon?: boolean;
-    type: "stage" | "stand" | "food" | "attraction" | "placeholder" | "unknown";
+    type:
+      | "stage"
+      | "stand"
+      | "food"
+      | "attraction"
+      | "hygiene"
+      | "placeholder"
+      | "unknown";
     interactive?: boolean;
+    /**
+     * List of calendar tags to filter by to show relevant events
+     */
     calendarQuery?: string[];
     padding?: number;
+    /**
+     * "Always" show label on map irregardless of zoom level
+     */
+    stickyLabel?: boolean;
+    /**
+     * Override automatic label width calculation to visually center, if needed
+     */
+    labelWidth?: number;
   };
 }
