f4c8cd28f0246954859d2946e94a79b795f1f3fb
niccofyren <niccofyren@gmail.com>
2025-12-01 13:30:22 +0100

    feat: tg26 launch (#244)

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,21 +4,21 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "#0E0F16",
-        backgroundSecondary: "#131518",
+        background: "#06070a",
+        backgroundSecondary: "#0E0F16",
       },
       typography: (theme) => ({
         DEFAULT: {
           css: {
             color: theme("colors.white"),
             a: {
-              color: theme("colors.blue.400"),
+              color: theme("colors.sky.400"),
               "&:hover": {
-                color: theme("colors.blue.600"),
+                color: theme("colors.sky.600"),
               },
             },
             "h1, h2, h3, h4": {
-              color: theme("colors.white"),
+              color: theme("colors.sky.400"),
             },
             code: {
               color: theme("colors.white"),
